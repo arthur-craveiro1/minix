12b9a279f0f43f7e2433309cdd333aeefece15e0
kmvd-mosselaar <109046097+kmvd-mosselaar@users.noreply.github.com>
2025-07-01 22:30:21 -0300

    atividade2 exec.c

--- minix/servers/pm/exec.c
+++ minix/servers/pm/exec.c
@@ -46,6 +46,7 @@ do_exec(void)
 	m.VFS_PM_PATH = (void *)m_in.m_lc_pm_exec.name;
 	m.VFS_PM_PATH_LEN = m_in.m_lc_pm_exec.namelen;
 	m.VFS_PM_FRAME = (void *)m_in.m_lc_pm_exec.frame;
+	printf("Executando: %s\n", exec_name);
 	m.VFS_PM_FRAME_LEN = m_in.m_lc_pm_exec.framelen;
 	m.VFS_PM_PS_STR = m_in.m_lc_pm_exec.ps_str;
 
