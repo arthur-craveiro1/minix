9a3dad7e755de07a02b791298eb28e8b6f293c9c
Luis-Menezes <luislipe4@gmail.com>
2025-07-03 12:40:22 -0300

    added more test acording to professor's suggestion

--- roda_teste.sh
+++ roda_teste.sh
@@ -4,12 +4,14 @@
 cc teste_processos.c -o teste_processos
 
 # Executa os testes com diferentes parâmetros e redireciona a saída para arquivos distintos
-./teste_processos 50 1000 100000 > resultados_01.txt
-./teste_processos 100 1000 100000 > resultados_02.txt
-./teste_processos 150 1000 100000 > resultados_03.txt
+./teste_processos 10 10000 1000000 > resultados_00.txt
+./teste_processos 50 10000 1000000 > resultados_01.txt
+./teste_processos 100 10000 1000000 > resultados_02.txt
+./teste_processos 150 10000 1000000 > resultados_03.txt
 
-./teste_processos 50 10000 1000000 > resultados_11.txt
-./teste_processos 100 10000 1000000 > resultados_12.txt
-./teste_processos 150 10000 1000000 > resultados_13.txt
+./teste_processos 10 100000 10000000 > resultados_10.txt
+./teste_processos 50 100000 10000000 > resultados_11.txt
+./teste_processos 100 100000 10000000 > resultados_12.txt
+./teste_processos 150 100000 10000000 > resultados_13.txt
 
 echo "Todos os testes foram executados com sucesso."
